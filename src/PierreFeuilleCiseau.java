122d68f25f652bf3d6530035295671492176e264
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-04-14 17:44:50 +0200

    Update PierreFeuilleCiseau.java

--- src/PierreFeuilleCiseau.java
+++ src/PierreFeuilleCiseau.java
@@ -5,6 +5,8 @@ public class PierreFeuilleCiseau {
 	public static void main(String[] args) {
 		
 		Rockpaperscissors.play();
+		
+		//Relancer le programme tant que le joueur choisi de recommencer une partie
 	
 		while(Rockpaperscissors.playagain())
 		{
